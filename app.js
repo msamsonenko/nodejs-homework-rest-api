52f372e94d9ebaf4a4f4a357ebf0a3dbeaa1605b
IrynaSam <52596926+msamsonenko@users.noreply.github.com>
2022-06-28 16:25:56 +0300

    finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -15,13 +16,14 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-	res.status(404).json({ message: "Not found" });
+	res.status(404).json({ message: "app: Not found" });
 });
 
 app.use((err, req, res, next) => {
--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,6 +1,7 @@
 const { Conflict } = require("http-errors");
 const { User } = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const signup = async (req, res) => {
 	const { name, email, password } = req.body;
@@ -9,10 +10,12 @@ const signup = async (req, res) => {
 		throw new Conflict("Email in use");
 	}
 	const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+	const avatarURL = gravatar.url(email);
 	const result = await User.create({
 		name,
 		email,
 		password: hashPassword,
+		avatarURL,
 	});
 	res.status(201).json({
 		status: "success",
@@ -22,6 +25,7 @@ const signup = async (req, res) => {
 				name,
 				email,
 				subscription: result.subscription,
+				avatarURL,
 			},
 		},
 	});
